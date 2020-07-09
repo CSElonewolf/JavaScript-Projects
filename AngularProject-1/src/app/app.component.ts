ad3270c10eaa85ccdea56ba9c3689a05760bb1f8
Devkant Gorai <51082429+CSElonewolf@users.noreply.github.com>
2020-07-09 20:10:24 +0530

    updated the files

--- AngularProject-1/src/app/app.component.ts
+++ AngularProject-1/src/app/app.component.ts
@@ -9,10 +9,17 @@ import { HttpClient } from '@angular/common/http'
 export class AppComponent {
   data: any = "";
   arrayvalue: any = [];
+  datetime:any="";
+  date:any="";
+  time:any="";
   constructor(public http: HttpClient) {
     this.http.get("https://api.covid19api.com/summary").subscribe((value: any) => {
       this.data = value.Global;
       this.arrayvalue = value.Countries;
+      this.datetime = value.Countries[0].Date;
+      this.date = this.datetime.substring(0,this.datetime.indexOf('T'));
+      this.time = this.datetime.substring(this.datetime.indexOf('T')+1,this.datetime.indexOf('Z'));
+      this.time+="UTC";
     })
   }
 }
